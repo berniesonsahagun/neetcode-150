c495a225e1fb63724e1c7a4de8bce7c6e36bfddf
Bernieson Sahagun <berniesonsahagun@gmail.com>
2025-07-28 15:23:55 +0800

    [REJECTED]: trapping rain water

---
+++ trapping-rain-water.js
@@ -0,0 +1,30 @@
+class Solution {
+  /**
+   * @param {number[]} height
+   * @return {number}
+   */
+  trap(height) {
+    let i = 0,
+      j = 1;
+    let water = 0;
+    let acc = 0;
+
+    while (j < height.length) {
+      if (height[j] >= height[i]) {
+        water += acc;
+        i = j;
+        acc = 0;
+      } else {
+        acc += height[i] - height[j];
+      }
+
+      j++;
+    }
+
+    return water;
+  }
+}
+
+const sol = new Solution();
+const res = sol.trap([0, 2, 0, 3, 1, 0, 1, 3, 2, 1]);
+console.log(res);
