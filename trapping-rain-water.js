b32dabef26da478874d15457623583b920ceaf3f
Bernieson Sahagun <berniesonsahagun@gmail.com>
2025-07-31 12:02:25 +0800

    [REJECTED]: trapping rain water

--- trapping-rain-water.js
+++ trapping-rain-water.js
@@ -26,5 +26,5 @@ class Solution {
 }
 
 const sol = new Solution();
-const res = sol.trap([0, 2, 0, 3, 1, 0, 1, 3, 2, 1]);
+const res = sol.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
 console.log(res);
